fdfe412218cf4fbae0f8ddf446df64f0e7d43fa0
Mikita Kazimirau <allwillburnn@gmail.com>
2023-02-23 19:54:17 +0300

    fix export

--- const/regExp.ts
+++ const/regExp.ts
@@ -1,3 +1,5 @@
-export const emailPattern: RegExp = /[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/gm;
-export const passwordPattern: RegExp = /(?=.*(.{8,}))(?=.*(\d))/gm;
-export const usernamePattern: RegExp = /.{1}/gm;
+export { emailPattern, passwordPattern, usernamePattern }
+
+const emailPattern: RegExp = /[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/gm;
+const passwordPattern: RegExp = /(?=.*(.{8,}))(?=.*(\d))/gm;
+const usernamePattern: RegExp = /.{1}/gm;
