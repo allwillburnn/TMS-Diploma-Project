6fe33c32264caa79ac70105f1054ffc9ef6ba9f8
Mikita Kazimirau <allwillburnn@gmail.com>
2023-02-25 18:27:55 +0300

    fix url (weird query in api)

--- data/api/api.ts
+++ data/api/api.ts
@@ -3,13 +3,10 @@ import axios, { AxiosResponse } from 'axios';
 const initialUrl: string = "https://jsonplaceholder.typicode.com";
 
 axios.defaults.validateStatus = (status) => {
-    console.log("Validating status:", status >= 200 && status <= 304);
     return status >= 200 && status < 500;
 };
 
 async function handleResponse(response: AxiosResponse<any>, expectedStatusCode?: number) {
-
-    console.log(response.status);
 
     // if (expectedStatusCode) {
     //     expect(response?.status).toBe(expectedStatusCode);
@@ -19,27 +16,29 @@ async function handleResponse(response: AxiosResponse<any>, expectedStatusCode?:
 
     // return response.data;
 
-    if (expectedStatusCode) {
+    if (expectedStatusCode && (response?.status === 200 || response?.status === 201)) {
         expect(response?.status).toBe(expectedStatusCode);
         return response.data;
     } else if (response?.status === 404) {
         return response?.status;
-    } else if (response?.status >= 400) {
+    } else if (response?.status >= 500) {
         throw new Error(`API response with status ${response?.status} error`);
     }
 
 }
 
-export async function getMethod(route: string, query?: object, expectedStatusCode?: number) {
-    const url: string = `${initialUrl}${route}${query}`;
-    const response: AxiosResponse = await axios.get(url);
+export async function getMethod(route: string, id?: number, query?: object, expectedStatusCode?: number) {
+    const url: string = `${initialUrl}${route}/${id}`;
+    // console.log(await axios.get(url, { params: query }));
+    // console.log(query);
+    const response: AxiosResponse = await axios.get(url, { params: query });
     return handleResponse(response, expectedStatusCode);
 }
 
 
 export async function postMethod(route: string, query?: object, expectedStatusCode?: number) {
     const url: string = `${initialUrl}${route}${query}`;
-    const response: AxiosResponse = await axios.get(url);
+    const response: AxiosResponse = await axios.get(url, { params: query });
     return handleResponse(response, expectedStatusCode);
 }
 
--- data/api/endpoints/getEndpoint.ts
+++ data/api/endpoints/getEndpoint.ts
@@ -1,13 +1,13 @@
 import { getMethod } from "../api";
 
 export async function getPostById(id: number, expectedStatusCode?: number): Promise<any> {
-    return await getMethod("/posts", { id: id }, expectedStatusCode);
+    return await getMethod("/posts", id, {}, expectedStatusCode);
 }
 
 export async function getAlbumById(id: number, expectedStatusCode?: number): Promise<any> {
-    return await getMethod("/albums", { id: id }, expectedStatusCode);
+    return await getMethod("/albums", id, {}, expectedStatusCode);
 }
 
 export async function getPhotoById(id: number, expectedStatusCode?: number): Promise<any> {
-    return await getMethod("/photos", { id: id }, expectedStatusCode);
+    return await getMethod("/photos", id, {}, expectedStatusCode);
 }
