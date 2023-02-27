9601e5feeb8b44eeb373b78908dd0617d3da9f1b
Mikita Kazimirau <allwillburnn@gmail.com>
2023-02-27 22:43:25 +0300

    initial setup

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+import createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', createBundler());
+    },
+    baseUrl: "https://www.onliner.by/",
+  },
+  watchForFileChanges: false,
+  defaultCommandTimeout: 10000,
+});
